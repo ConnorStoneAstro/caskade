97169f8b1ee3f0c89b48f3b85f37ed5eedee17c8
Connor Stone <connorstone628@gmail.com>
2025-01-14 10:45:57 -0500

    add override test

--- tests/test_context.py
+++ tests/test_context.py
@@ -1,4 +1,4 @@
-from caskade import Module, Param, forward, ActiveContext, ValidContext
+from caskade import Module, Param, forward, ActiveContext, OverrideParam
 import torch
 
 
@@ -35,3 +35,27 @@ def test_active_context():
             assert not testsim.c.active
             assert testsim.testfunc(params2).item() == 10.0
         assert testsim.testfunc().item() == 6.0
+
+
+def test_override_param():
+
+    class TestSim(Module):
+        def __init__(self):
+            super().__init__()
+            self.a = Param("a", 3.0)
+            self.b = Param("b", lambda p: p["a"].value)
+            self.b.link(self.a)
+            self.c = Param("c", None)
+            self.a_vals = (torch.tensor(1.0), torch.tensor(2.0))
+
+        @forward
+        def testfunc(self, a, b, c):
+            d = a + b + c
+            with OverrideParam(self.a, self.a_vals[0]):
+                d = d + self.b.value
+            with OverrideParam(self.a, self.a_vals[1]):
+                d = d + self.b.value
+            return d
+
+    testsim = TestSim()
+    assert testsim.testfunc(torch.tensor([5.0])).item() == 14.0
