43c91766d1df72a8997cb9926a33130a2d441838
Connor Stone <connorstone628@gmail.com>
2026-01-30 12:12:16 -0500

    still trying to make windows not be terrible

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,17 +1,14 @@
 import os
 import sys
 import pytest
+from caskade import backend
+import numpy as np
 
-if "win" in sys.platform:
-
-    def dummyfork():
-        return 123456789
-
-    os.fork = dummyfork
+if sys.platform.startswith("win"):
+    pytest.skip(reason="Windows cant run these tests")
 
 
 # This forces the test to run in a separate subprocess
-@pytest.mark.skipif("win" in sys.platform.lower(), reason="does not run on windows")
 @pytest.mark.filterwarnings("ignore:os.fork()")
 @pytest.mark.filterwarnings("ignore:This Process")
 @pytest.mark.forked
@@ -28,3 +25,16 @@ def test_missing_jax(monkeypatch):
     import caskade as ck
 
     assert ck.utils.jnp is None
+
+
+def test_broadcast_cat():
+    with pytest.raises(ValueError):
+        backend.broadcast_cat(())
+
+    arr1 = backend.as_array(1.0)
+    arr2 = backend.as_array(np.ones((2, 2)))
+    arr3 = backend.as_array(np.ones((3, 2, 3)))
+    assert backend.broadcast_cat((arr1, arr2, arr3)).shape == (3, 2, 6)
+
+    with pytest.raises(ValueError):
+        backend.broadcast_cat((arr1, arr2, arr3), dim=10)
